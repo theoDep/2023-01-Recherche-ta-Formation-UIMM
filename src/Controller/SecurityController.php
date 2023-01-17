1d69fe28ad9bb628e25f8a1ca629f2b7723fe9af
Spacantho <devbox1001@gmail.com>
2023-01-17 15:23:35 +0100

    fix

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, UserPasswordEncoderInterface $encoder): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
