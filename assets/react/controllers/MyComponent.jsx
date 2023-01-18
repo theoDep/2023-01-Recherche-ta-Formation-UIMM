0e2f90bc8849a9d358ff47ee000da2155d672cad
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2023-01-18 11:29:55 +0100

    Install and configure React with Symfony UX

---
+++ assets/react/controllers/MyComponent.jsx
@@ -0,0 +1,10 @@
+// assets/react/controllers/MyComponent.jsx
+import React from "react";
+
+export default function MyComponent(props) {
+  return (
+    <div>
+      <h1>Welcome Home {props.name}</h1>
+    </div>
+  );
+}
