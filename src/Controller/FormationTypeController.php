5935a220fdb328aad9754ba524ada52fb3be58e5
Kilian <ide.kilian02@gmail.com>
2023-01-19 13:24:45 +0100

    Create Form/FormationType & FormationTypeController

---
+++ src/Controller/FormationTypeController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Formation;
+use App\Form\FormationType;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/formation/type')]
+class FormationTypeController extends AbstractController
+{
+    #[Route('/', name: 'app_formation_type_index', methods: ['GET'])]
+    public function index(FormationRepository $formationRepository): Response
+    {
+        return $this->render('formation_type/index.html.twig', [
+            'formations' => $formationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_formation_type_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, FormationRepository $formationRepository): Response
+    {
+        $formation = new Formation();
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formationRepository->save($formation, true);
+
+            return $this->redirectToRoute('app_formation_type_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('formation_type/new.html.twig', [
+            'formation' => $formation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_formation_type_show', methods: ['GET'])]
+    public function show(Formation $formation): Response
+    {
+        return $this->render('formation_type/show.html.twig', [
+            'formation' => $formation,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_formation_type_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Formation $formation, FormationRepository $formationRepository): Response
+    {
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formationRepository->save($formation, true);
+
+            return $this->redirectToRoute('app_formation_type_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('formation_type/edit.html.twig', [
+            'formation' => $formation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_formation_type_delete', methods: ['POST'])]
+    public function delete(Request $request, Formation $formation, FormationRepository $formationRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$formation->getId(), $request->request->get('_token'))) {
+            $formationRepository->remove($formation, true);
+        }
+
+        return $this->redirectToRoute('app_formation_type_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/FormationType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('studies_level')
+            ->add('codes')
+            ->add('jobs')
+            ->add('conditions')
+            ->add('costs')
+            ->add('locations')
+            ->add('sequels')
+            ->add('format')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
