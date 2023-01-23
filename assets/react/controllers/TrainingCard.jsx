dbc392453019ac7cf9569814075ff26952e0d280
Spacantho <devbox1001@gmail.com>
2023-01-23 11:13:18 +0100

    assemblage

--- assets/react/controllers/TrainingCard.jsx
+++ assets/react/controllers/TrainingCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 
 export default ({ training }) => {
-  const { name, description, studiesLevel, id } = training;
+  const { name, descriptive, studiesLevel, id } = training;
   return (
     <Card className="cardFormation mx-3 px-0">
       <Card.Header className="w-100 bg-primary text-white">
@@ -10,7 +10,7 @@ export default ({ training }) => {
       </Card.Header>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Text>{description}</Card.Text>
+        <Card.Text className='cardLineClamp'>{descriptive}</Card.Text>
         <Card.Link href={`/training/${id}`}>En savoir plus</Card.Link>
       </Card.Body>
     </Card>
